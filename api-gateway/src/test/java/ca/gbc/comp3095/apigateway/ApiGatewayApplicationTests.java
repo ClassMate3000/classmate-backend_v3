26963e1aa186068215058306d1e410e19da94dc4
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-02 16:05:39 -0400

    Cleaning up api-gateway tests - debugging final issues

--- api-gateway/src/test/java/ca/gbc/comp3095/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/ca/gbc/comp3095/apigateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,19 @@
 package ca.gbc.comp3095.apigateway;
 
-/*
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+// @Disabled("Disabled: no datasource configured for tests")
+@ActiveProfiles("test")
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "jwt.secret=12345678901234567890123456789012", // 32 chars for HS256,
+        "spring.flyway.enabled=false"
+})
 class ApiGatewayApplicationTests {
 
 	@Test
@@ -12,4 +21,3 @@ class ApiGatewayApplicationTests {
 	}
 
 }
-*/
--- api-gateway/src/test/java/ca/gbc/comp3095/apigateway/config/GatewayRoutingConfigTest.java
+++ api-gateway/src/test/java/ca/gbc/comp3095/apigateway/config/GatewayRoutingConfigTest.java
@@ -1,6 +1,5 @@
 package ca.gbc.comp3095.apigateway.config;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -11,7 +10,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-@ActiveProfiles("test")
+@ActiveProfiles("test") // Loads src/test/resources/application-test.properties
 @AutoConfigureWebTestClient
 class GatewayRoutingConfigTest {
 
@@ -27,15 +26,15 @@ class GatewayRoutingConfigTest {
                 .is5xxServerError(); // backend not running → proves route exists
     }
 
-/*
+
     @Test
     void testTasksRouteExists() {
         webTestClient.get()
                 .uri("/api/v1/tasks/test")
                 .exchange()
                 .expectStatus()
-                .is5xxServerError();
+                .is5xxServerError(); // backend not running → proves route exists
     }
 
- */
+
 }
